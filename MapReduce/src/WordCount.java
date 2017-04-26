c7abda332c76ea206fa7c8b1559ae2724b42e0f6
blupiac <blupiac@gmail.com>
2017-04-26 22:27:46 +0200

    remove numbers and symbols from words

--- MapReduce/src/WordCount.java
+++ MapReduce/src/WordCount.java
@@ -1,11 +1,16 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.Normalizer;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 
@@ -15,7 +20,7 @@ public class WordCount {
 	private HashMap<String, Integer> count = new HashMap<String, Integer>();
 
 	// http://www.java67.com/2015/01/how-to-sort-hashmap-in-java-based-on.html
-	TreeMap<String, Integer> sorted;
+	LinkedHashMap<String, Integer> sorted;
 	Set<Entry<String, Integer>> mappings;
 	
 	public WordCount(String path) {
@@ -28,21 +33,36 @@ public class WordCount {
 
 		countWords();
 		
-		sorted = new TreeMap<>(count);
-		mappings = sorted.entrySet();
+		// Sort method needs a List, so let's first convert Set to List in Java 
+		List<Entry<String, Integer>> listOfEntries = new ArrayList<Entry<String, Integer>>(count.entrySet());
+		
+		// sorting HashMap by values using comparator 
+		Collections.sort(listOfEntries, valueComparator);
+		sorted = new LinkedHashMap<String, Integer>(listOfEntries.size());
+		
+		// copying entries from List to Map 
+		for(Entry<String, Integer> entry : listOfEntries)
+		{
+			sorted.put(entry.getKey(), entry.getValue());
+		}
+		
 	}
 	
 	Comparator<Entry<String, Integer>> valueComparator = new Comparator<Entry<String,Integer>>()
-			{ 
-				@Override
-				public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2)
-				{
-					int v1 = e1.getValue();
-					int v2 = e2.getValue();
-					if (v1 < v2)	return 0;
-					else			return 1;
-				}
-			};
+	{ 
+		@Override
+		public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2)
+		{
+			int v1 = e1.getValue();
+			int v2 = e2.getValue();
+			String k1 = e1.getKey();
+			String k2 = e2.getKey();
+			if (v1 < v2)					return 0;
+			else if (v1 > v2)				return 1;
+			else if(k1.compareTo(k2) < 0)	return 0;	// v1 == v2
+			else							return 1;
+		}
+	};
 
 	// http://stackoverflow.com/questions/326390/how-do-i-create-a-java-string-from-the-contents-of-a-file
 	private static String readFile(String path) 
@@ -58,27 +78,38 @@ public class WordCount {
 
 		for(int i = 0; i < words.length; i++)
 		{
-			// http://stackoverflow.com/questions/21946042/remove-all-spaces-and-punctuation-anything-not-a-letter-from-a-string
-			String word = words[i].replaceAll("[^A-Za-z]+", "").toLowerCase();
+			// http://stackoverflow.com/questions/1611979/remove-all-non-word-characters-from-a-string-in-java-leaving-accented-charact
+			String word = words[i].replaceAll("[^\\p{L}\\p{Nd}]+", "").toLowerCase();
+			word = word.replaceAll("^[\\s\\.\\d]+", "");
 			
-			if(count.containsKey(word))
+			if(count.containsKey(word) && !word.matches(".*\\d+.*"))
 			{
 				count.put(word, count.get(word) + 1);
 			}
-			else
+			else if(!word.matches(".*\\d+.*"))
 			{
 				count.put(word, 1);
 			}
 		}
 	}
-
+	
+	// http://stackoverflow.com/questions/1453171/remove-diacritical-marks-
+	// %C5%84-%C7%B9-%C5%88-%C3%B1-%E1%B9%85-%C5%86-%E1%B9%87-%E1%B9%8B-%E1%B9%89-%CC%88-%C9%B2-%C6%9E-%E1%B6%87-%C9%B3-%C8%B5-from-unicode-chars
+/*	public static final Pattern DIACRITICS_AND_FRIENDS
+	    = Pattern.compile("[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+");
+	
+	private static String stripDiacritics(String str) {
+	    str = Normalizer.normalize(str, Normalizer.Form.NFD);
+	    str = DIACRITICS_AND_FRIENDS.matcher(str).replaceAll("");
+	    return str;
+	}
+*/
 	public void showResult(boolean ordered)
 	{
 		if(ordered)
 		{
-			for(Entry<String, Integer> mapping : mappings)
-			{
-				System.out.println(mapping.getKey() + " : " + mapping.getValue());
+			for (String name: sorted.keySet()){
+				System.out.println(name + " : " + count.get(name));
 			}
 		}
 		else
